0400af19e2928dfdc7ede489be691ee1390870fa
Polaroid339 <victor339335@gmail.com>
2025-03-15 17:31:23 -0300

    feat: Classe Aluno, Professor e Escola

---
+++ src/main/java/com/senai/senaijavagradle/model/Escola.java
@@ -0,0 +1,6 @@
+public class Escola{
+    private int Id;
+    private String Nome;
+    private String Cnpj;
+    private String Email;
+}
---
+++ src/main/java/com/senai/senaijavagradle/model/Professor.java
@@ -0,0 +1,6 @@
+public class Professor{
+    private int Id;
+    private String Nome;
+    private String CPF;
+    private String Email;
+}
