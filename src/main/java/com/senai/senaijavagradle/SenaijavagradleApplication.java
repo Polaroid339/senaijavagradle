7a708d679753aee9364074f79db2acff5bf4ab24
Polaroid339 <victor339335@gmail.com>
2025-03-15 17:08:37 -0300

    feat: Sprong Gradle

---
+++ src/main/java/com/senai/senaijavagradle/SenaijavagradleApplication.java
@@ -0,0 +1,13 @@
+package com.senai.senaijavagradle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SenaijavagradleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SenaijavagradleApplication.class, args);s
+	}
+
+}
